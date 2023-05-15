824a2d8f284525a5beae19fc974d286f197e3b36
mohamed adel <muhammedadeltaha@gmail.com>
2023-05-15 16:10:51 +0300

    Time taken for operations

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
-import java.util.ArrayList;
-
 public class Main {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        NSquareSpaceHash hash = new NSquareSpaceHash();
+        long start, end;
+        start = System.currentTimeMillis();
+        for (int i = 0; i < 1000_000; i++){
+            hash.insert(i);
+        }
+        end = System.currentTimeMillis();
+        System.out.println("insert time = " + (end - start));
+
+        start = System.currentTimeMillis();
+        for (int i = 0; i < 1000_000; i++){
+            hash.search(i);
+        }
+        end = System.currentTimeMillis();
+        System.out.println("search time = " + (end - start));
+
+        start = System.currentTimeMillis();
+        for (int i = 0; i < 1000_000; i++){
+            hash.delete(i);
+        }
+        end = System.currentTimeMillis();
+        System.out.println("delete time = " + (end - start));
+    }
 }
